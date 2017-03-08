799fc16c07c8aeb5e271396a6a375f390f4eb007
katherine95 <kathiekim95@gmail.com>
2017-03-08 11:23:01 +0300

    student portal class

--- oop.py
+++ oop.py
@@ -1,48 +1,18 @@
-class BankAccount(object):
-  def withdraw(self):
+class Student(object):
+  def regno(self):
     pass
-  def deposit(self):
+  def course(self):
     pass
-'''Inheritance; savings class is a class within a parent class'''
-'''it inherits features of the main class, BankAccount class'''
-class SavingsAccount(BankAccount):
-    '''encapsulation; methods of withdraw and deposit are only used within the savings class. they are not visible to other classes'''
+'''Inheritance; portal class is a class within a parent class(student)'''
+'''it inherits features of the main class, student class'''
+class portal(student):
+    '''encapsulation; methods of regno and course are only used within the portal class. they are not visible to other classes'''
   def __init__(self):
-    self.balance = 500
 
-  def deposit(self, amount):
-    if (amount < 0):
-      return "Invalid deposit amount"
-    else:
-      self.balance =self.balance + amount
-      return self.balance
+  def regno(self):
 
-  def withdraw(self, amount):
-    if ( 500< amount < 0):
-      return "Invalid withdraw amount"
-    elif (amount >= self.balance ):
-      return "Cannot withdraw beyond the current account balance"
-    elif (self.balance - amount <= 500):
-      return "Cannot withdraw beyond the minimum account balance"
-    else:
-      self.balance = self.balance - amount
-      return self.balance
+      return
 
-class CurrentAccount(BankAccount):
-  def __init__(self):
-    self.balance = 0
-  def deposit(self, amount):
-    if (amount < 0):
-      return "Invalid deposit amount"
-    else:
-      self.balance = self.balance + amount
-      return self.balance
+  def course(self):
 
-  def withdraw(self, amount):
-    if (amount < 0):
-      return "Invalid withdraw amount"
-    elif (amount >= self.balance ):
-      return "Cannot withdraw beyond the current account balance"
-    else:
-      self.balance = self.balance - amount
-      return self.balance
+      return

fb5f23cd035aa46d9140fc65b4240f98df6ed0de
katherine95 <kathiekim95@gmail.com>
2017-03-09 16:13:11 +0300

    oop class

--- oop.py
+++ oop.py
@@ -6,13 +6,17 @@ class Student(object):
 '''Inheritance; portal class is a class within a parent class(student)'''
 '''it inherits features of the main class, student class'''
 class portal(student):
-    '''encapsulation; methods of regno and course are only used within the portal class. they are not visible to other classes'''
-  def __init__(self):
+'''encapsulation; methods of regno and course are only used within the portal class. they are not visible to other classes'''
+  def __init__(self,fee,units):
+    self.fee=fee
+    self.units=units
 
   def regno(self):
-
-      return
+    if self.fee is not None:#check if fee is none
+      return 0
+    return 5000#return fees apid
 
   def course(self):
-
-      return
+    if self.units is None:
+      return 'not a student'#check if units are none
+    return units#return registered units
